091d6814b34ce9bc8f0338eabc46599b459957af
Roman Khateev <toolooppp@mail.ru>
2023-09-04 12:23:49 +0300

    Hello World!

--- myprogramm.py
+++ myprogramm.py
@@ -19,12 +19,4 @@ memoryview
 
 '''
 
-from datetime import datetime
-
-dt_now = f'{datetime.now()}'
-dt_now = dt_now.split('-')[0]
-
-birth_year = int(input('Введите год рождения: '))
-real_age = int(dt_now) - birth_year
-
-print(real_age)
+print('Hello world!')
